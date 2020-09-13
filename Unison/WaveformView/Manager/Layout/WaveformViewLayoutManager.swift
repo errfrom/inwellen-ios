b063cce77c1bef84416d7266830a773614185070
Dmitri Schuiski <errfrom@yandex.ru>
2020-09-13 20:47:54 +0300

    [+] Initial commit.

---
+++ Unison/WaveformView/Manager/Layout/WaveformViewLayoutManager.swift
@@ -0,0 +1,9 @@
+//
+//  WaveformViewLayoutManager.swift
+//  Unison
+//
+//  Created by Dzmitry Shuysky on 9/13/20.
+//  Copyright © 2020 Unison. All rights reserved.
+//
+
+import Foundation
---
+++ Unison/WaveformView/View/WaveformView.swift
@@ -0,0 +1,9 @@
+//
+//  WaveformView.swift
+//  Unison
+//
+//  Created by Dzmitry Shuysky on 9/13/20.
+//  Copyright © 2020 Unison. All rights reserved.
+//
+
+import Foundation
